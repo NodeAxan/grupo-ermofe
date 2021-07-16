a8447804b0ffec91469264bced6fb289d1230df9
Daniel Morales <daniel.cmorales3@gmail.com>
2021-07-16 09:43:33 -0500

    Mostrar entradas de blog

--- src/app/pipes/text-transform-pipe.pipe.ts
+++ src/app/pipes/text-transform-pipe.pipe.ts
@@ -6,14 +6,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TextTransformPipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
+    console.log(args);
+
     const div = document.createElement('div');
     div.innerHTML = value.trim();
     const images = div.getElementsByTagName("img");
     for (let i = 0; i < images.length; i++) {
-        const image = images[i];
-        const imageSrc = image.getAttribute("src");
-        image.src = "http://ws.grupoermofe.com" + imageSrc;
+      const image = images[i];
+      const imageSrc = image.getAttribute("src");
+      image.src = "http://ws.grupoermofe.com" + imageSrc;
     }
+
+    if (args.length) {
+      if (args[0]) {
+        const block = div.querySelector('blockquote');
+        let description = block.querySelector('p').textContent;
+        description = description.substr(0, 150);
+        block.innerHTML = `<p>${description}...</p>`;
+      }
+    }
+
     return div.outerHTML;
   }
 
