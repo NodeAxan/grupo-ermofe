5a6f004e1eab29ce96eb6dd220ae372aa4260cb2
Daniel Morales <daniel.cmorales3@gmail.com>
2021-08-30 11:21:14 -0500

    change about info

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -10,26 +10,26 @@ export class AboutComponent implements OnInit {
   public timeline: { title: string, text: string, img?: string }[] = [
     {
       title: '1990',
-      text: 'Nuestro fundador después de graduarse de ingeniero civil, comenzó a trabajar con su papá, quien desde pequeño inculcó, el gusto por esta profesión, realizando obras como puentes tubulares y ploteos.',
+      text: 'Nuestro fundador, al graduarse de ingeniero civil, comenzó a trabajar con su papá quien desde pequeño inculcó en él, el gusto por esta profesión, realizando obras como puentes tubulares y piloteos.',
     },
     {
       title: '1993',
-      text: 'Decidió emprender por su cuenta, realizando diferentes tipos de proyectos para el sector público y privado.',
+      text: 'Nuestro fundador decidió emprender por su cuenta realizando diferentes tipos de proyectos para el sector público y privado.',
       img: '../../../assets/img/banners/banner-contact.jpg'
     },
     {
       title: '2003',
-      text: 'Por la alta demanda de obras en el estado de Tabasco, que constituye formalmente GRUPO ERMOFE. Realizando puentes, escuelas y obras privadas.',
+      text: 'Por la alta demanda de obras en el estado de Tabasco constituye formalmente junto con su esposa, GRUPO ERMOFE, realizando proyectos como puentes, escuelas y obras privadas.',
       img: '../../../assets/img/banners/banner-contact.jpg'
     },
     {
       title: '2012',
-      text: 'Nuestro director operativo al graduarse de ingeniero civil, comienza a trabajar en empresas privadas en el estado de Jalisco, adquiriendo conocimientos y experiencia.',
+      text: 'Nuestro Director Operativo decide seguir los pasos de su padre y, al graduarse de ingeniero civil, comienza a trabajar en empresas privadas en el estado de Jalisco, adquiriendo conocimientos y experiencia.',
       img: '../../../assets/img/banners/banner-contact.jpg'
     },
     {
       title: '2014',
-      text: 'Su motivación por el crecimiento de la empresa familiar, lo trajo nuevamente a Tabasco y trabajar de la mano con nuestro fundador, con el objetivo de transformar la movilidad vial y peatonal, ofreciendo a la industria de la construcción, productos que otorgan un gran costo beneficio, tanto al cliente como al usuario.',
+      text: 'Su motivación por el crecimiento de la empresa familiar, lo trajo nuevamente a Tabasco a trabajar de la mano con nuestro fundador con el objetivo de transformar la movilidad vial y peatonal, integrando a la industria de la construcción productos que otorgan un gran costo beneficio, tanto al cliente como al usuario.',
     },
     {
       title: '2015',
@@ -37,7 +37,7 @@ export class AboutComponent implements OnInit {
     },
     {
       title: '2019',
-      text: 'Instalamos 10 proyectos de aluminio marino, entre ellos el puente más largo sin apoyo intermedio y parapetos de aluminio marino en el puente “La Unidad” en Campeche, posicionándonos este año al frente del mercado.',
+      text: 'Instalamos más de 10 estructuras de aluminio marino, entre ellas el puente más largo sin apoyo intermedio, y los parapetos del nuevo puente la unidad, posicionándonos este año al frente del mercado.',
       img: '../../../assets/img/banners/banner-contact.jpg'
     }
   ];
