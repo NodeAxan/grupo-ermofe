4254f354fa1db07a2619ce7bff514a72e1bfdd6e
Axel León <yka992@hotmail.com>
2021-10-26 08:48:55 -0500

    Environment handler

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,6 @@
+// Copy this file to environments/environment.ts with your real values
+
+export default {
+    API_KEY: "your-cockpit-api-key",
+    NODE_ENV: "production" // production, test or development
+}
