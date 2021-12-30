2d58e17bff4ed2cb6036a7b3e605b2e70e32b5f9
Axel León <yka992@hotmail.com>
2021-12-30 15:52:00 -0600

    Clients now comes from Cockpit

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -1,24 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { ClientService } from '../../services/client.service';
-import { api } from 'src/app/global/variable.global';
+import { Component, OnInit } from "@angular/core";
+import { ClientService } from "../../services/client.service";
+import { api } from "src/app/global/variable.global";
+import { ClientEntry } from "../../../interfaces/client";
 
 @Component({
-  selector: 'app-clients',
-  templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.scss'],
-  providers: [ClientService]
+  selector: "app-clients",
+  templateUrl: "./clients.component.html",
+  styleUrls: ["./clients.component.scss"],
+  providers: [ClientService],
 })
 export class ClientsComponent implements OnInit {
-
-  public clients: any;
-  public urlImage: string;
+  public clients: ClientEntry[];
+  public url = api.cockpit;
   public loading = false;
 
-  constructor(
-    private clientService: ClientService
-  ) {
-    this.urlImage = `${api.url}/img`;
-  }
+  constructor(private clientService: ClientService) {}
 
   ngOnInit() {
     this.getClients();
@@ -26,15 +22,14 @@ export class ClientsComponent implements OnInit {
 
   getClients() {
     this.clientService.getClients().subscribe(
-      result => {
+      (result) => {
         this.loading = true;
-        this.clients = result;
+        this.clients = result.entries;
       },
-      error => {
-        this.clients = false;
+      (error) => {
+        this.clients = [];
         console.log(error);
       }
     );
   }
-
 }
